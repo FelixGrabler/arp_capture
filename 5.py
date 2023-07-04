43ffaa2a3a2126ecd8849f9de476b9e30828c60a
FelixGrabler <felix.grabler@eet.energy>
2023-07-04 15:49:13 +0200

    added python code files that display a mac.db as a graph in 5 and 30 minute steps

---
+++ 5.py
@@ -0,0 +1,89 @@
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from mac_vendor_lookup import MacLookup
+from datetime import datetime
+
+DATABASE = "C:/Users/user/Documents/ARP/mac.db"
+
+
+def lookup_mac_address(mac):
+    vendor = None
+    try:
+        vendor = MacLookup().lookup(mac)
+    finally:
+        return vendor if vendor else mac
+
+
+def format_timestamp(timestamp):
+    dt = datetime.strptime(timestamp, "%Y%m%d%H%M%S")
+    return dt.strftime("%d.%m %H:%M")
+
+
+def get_sorted_mac_vendors(df):
+    unique_mac_addresses = df["address"].unique()
+    vendors = [lookup_mac_address(mac) for mac in unique_mac_addresses]
+    sorted_indices = np.argsort(vendors)
+    
+    return unique_mac_addresses[sorted_indices], np.array(vendors)[sorted_indices]
+
+
+def get_presence_matrix(df_sorted):
+    # Pivot table based on address and timestamp, values don't matter since we only care about presence, hence use address itself
+    df_pivot = df_sorted.pivot(index="address", columns="timestamp", values="address")
+    
+    # Create a binary matrix indicating presence (1) or absence (nan) of MAC address at each timestamp
+    presence_matrix = ~df_pivot.isnull()
+
+    return presence_matrix
+
+
+def plot_mac_presence(presence_matrix_sorted, sorted_mac_addresses, sorted_vendors, timestamps):
+    plt.figure(figsize=(10, 6))
+    plt.imshow(presence_matrix_sorted, cmap="Blues", aspect="auto")
+
+    # Add an almost transparent grid
+    plt.grid(color="gray", linestyle="--", alpha=0.2)
+
+    plt.title("Presence of MAC Addresses at Different Timestamps")
+    plt.xlabel("Timestamp")
+    plt.ylabel("MAC Address")
+
+    # Set custom tick labels for y-axis (MAC addresses)
+    plt.yticks(np.arange(len(sorted_mac_addresses)), sorted_vendors)
+
+    # Set custom tick labels for x-axis (timestamps)
+    x_ticks = np.arange(len(timestamps))
+    formatted_labels = [format_timestamp(timestamp) for timestamp in timestamps]
+    plt.xticks(x_ticks, formatted_labels, rotation=45, ha="right")
+
+    # Set spacings
+    plt.subplots_adjust(left=0.17, bottom=0.1, right=0.99, top=0.975)
+
+    plt.show()
+
+
+def analyze_db():
+    # Connect to the SQLite database
+    with sqlite3.connect(DATABASE) as conn:
+        # Read the data into a pandas DataFrame
+        df = pd.read_sql_query("SELECT * FROM mac_addresses", conn)
+
+    df_sorted = df.sort_values(by="address")
+
+    sorted_mac_addresses, sorted_vendors = get_sorted_mac_vendors(df_sorted)
+
+    presence_matrix = get_presence_matrix(df_sorted)
+
+    # Sort timestamps in ascending order (latest to newest) and sort presence matrix based on sorted timestamps
+    timestamps = sorted(df_sorted["timestamp"].unique())
+    presence_matrix_sorted = presence_matrix[timestamps]
+
+    plot_mac_presence(
+        presence_matrix_sorted, sorted_mac_addresses, sorted_vendors, timestamps
+    )
+
+
+if __name__ == "__main__":
+    analyze_db()
