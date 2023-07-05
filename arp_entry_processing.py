5c5c7085240a5ec76863db02ba4153cf47fee342
FelixGrabler <felix.grabler@eet.energy>
2023-07-05 14:05:32 +0200

    python now logs, uses ARP request and reply

--- arp_entry_processing.py
+++ arp_entry_processing.py
@@ -1,21 +1,47 @@
 import os
 import sqlite3
-from scapy.all import *
+from scapy.all import ARP, rdpcap
 from datetime import datetime, timedelta
 import pandas as pd
+import logging
+from logging.handlers import RotatingFileHandler
 
 DATABASE = '/etc/arp_capture/mac.db'
 PCAP_DIR = '/etc/arp_capture/pcap_files/'
+LOG_DIR = '/etc/arp_capture/logs'
+
+# Check if the log directory exists, if not, create it
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+# Setting up logging
+log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+log_file = os.path.join(LOG_DIR, 'arp_capture.log')
+
+# Use a rotating file handler to limit file size and number of backup log files
+handler = RotatingFileHandler(log_file, mode='a', maxBytes=100*1024, backupCount=2, encoding=None, delay=0)
+handler.setFormatter(log_formatter)
+handler.setLevel(logging.INFO)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+logger.addHandler(handler)
+
 
 def initialize_db():
     with sqlite3.connect(DATABASE) as conn:
-        conn.execute("""
+        conn.execute(
+            """
             CREATE TABLE IF NOT EXISTS mac_addresses (
                 timestamp TEXT,
                 address TEXT,
                 PRIMARY KEY (timestamp, address)
             )
-        """)
+        """
+        )
+
 
 def round_up_to_nearest_half_hour(dt):
     if dt.minute % 30 or dt.second:
@@ -23,9 +49,13 @@ def round_up_to_nearest_half_hour(dt):
     else:
         return dt
 
+
 def extract_timestamp(filename):
     basename = os.path.basename(filename)
-    return datetime.strptime(basename[4:18], "%Y%m%d%H%M%S")  # Convert to datetime object
+    return datetime.strptime(
+        basename[4:18], "%Y%m%d%H%M%S"
+    )  # Convert to datetime object
+
 
 def process_pcap_file(filename):
     timestamp = extract_timestamp(filename)
@@ -33,10 +63,14 @@ def process_pcap_file(filename):
     try:
         packets = rdpcap(filename)
     except Exception as e:
-        print("ERROR: Failed to read pcap file {}: {}".format(filename, e))
+        logging.error(f"Failed to read pcap file {filename}: {e}")
         return
 
-    mac_addresses = set(packet[ARP].hwsrc for packet in packets if ARP in packet and packet[ARP].op == 2)
+    mac_addresses = set(
+        packet[ARP].hwsrc
+        for packet in packets
+        if ARP in packet and (packet[ARP].op in [1, 2])
+    )
 
     if mac_addresses:
         timestamp = round_up_to_nearest_half_hour(timestamp)
@@ -44,14 +78,20 @@ def process_pcap_file(filename):
             cursor = conn.cursor()
             for address in mac_addresses:
                 try:
-                    cursor.execute("INSERT OR IGNORE INTO mac_addresses (timestamp, address) VALUES (?, ?)", (str(timestamp), address))
+                    cursor.execute(
+                        "INSERT OR IGNORE INTO mac_addresses (timestamp, address) VALUES (?, ?)",
+                        (str(timestamp), address),
+                    )
                 except sqlite3.IntegrityError:
                     continue  # Ignore duplicates
     try:
         os.remove(filename)
-        print("Deleted processed file: {}".format(filename))
+        logging.info(
+            f"Deleted processed file: {filename}. Processed MAC addresses: {len(mac_addresses)}"
+        )
     except Exception as e:
-        print("ERROR: Failed to delete processed file {}: {}".format(filename, e))
+        logging.error(f"Failed to delete processed file {filename}: {e}")
+
 
 def fill_gaps():
     with sqlite3.connect(DATABASE) as conn:
@@ -69,7 +109,9 @@ def fill_gaps():
             if last_row is None:
                 new_rows.append(row)
             elif (
-                30*60 <= (row["timestamp"] - last_row["timestamp"]).total_seconds() <= 2*60*60
+                30 * 60
+                <= (row["timestamp"] - last_row["timestamp"]).total_seconds()
+                <= 2 * 60 * 60
             ):
                 fill_timestamps = pd.date_range(
                     start=last_row["timestamp"] + timedelta(minutes=30),
@@ -89,21 +131,31 @@ def fill_gaps():
 
     with sqlite3.connect(DATABASE) as conn:
         cursor = conn.cursor()
-        for index, row in new_df.iterrows():
+        for _, row in new_df.iterrows():
             try:
-                cursor.execute("INSERT OR IGNORE INTO mac_addresses (timestamp, address) VALUES (?, ?)", (str(row["timestamp"]), row["address"]))
+                cursor.execute(
+                    "INSERT OR IGNORE INTO mac_addresses (timestamp, address) VALUES (?, ?)",
+                    (str(row["timestamp"]), row["address"]),
+                )
             except sqlite3.IntegrityError:
                 continue  # Ignore duplicates
 
-def process_pcap_files():
-    pcap_files = sorted(filename for filename in os.listdir(PCAP_DIR) if filename.startswith('arp_') and filename.endswith('.pcap'))
 
+def process_pcap_files():
+    pcap_files = sorted(
+        filename
+        for filename in os.listdir(PCAP_DIR)
+        if filename.startswith("arp_") and filename.endswith(".pcap")
+    )
+
+    # Exclude the last file because it is still written to
     pcap_files = pcap_files[:-1]
 
     for filename in pcap_files:
         process_pcap_file(os.path.join(PCAP_DIR, filename))
 
     fill_gaps()
+
 
 if __name__ == "__main__":
     initialize_db()
