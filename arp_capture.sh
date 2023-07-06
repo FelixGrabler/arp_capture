37ad82a3974abe54df0d84154549dfbacd43a28f
FelixGrabler <felix.grabler@eet.energy>
2023-07-06 10:56:24 +0200

    exclude db file from commit, changed sh script

--- arp_capture.sh
+++ arp_capture.sh
@@ -3,11 +3,8 @@
 # Create a directory to store the pcap files if it does not exist
 mkdir -p /etc/arp_capture/pcap_files
 
-while true
-do
-    TIMESTAMP=$(/bin/date +%Y%m%d%H%M%S)
-    FILENAME="/etc/arp_capture/pcap_files/arp_$TIMESTAMP.pcap"
+TIMESTAMP=$(/bin/date +%Y%m%d%H%M%S)
+FILENAME="/etc/arp_capture/pcap_files/arp_$TIMESTAMP.pcap"
 
-    # Capture ARP packets for 5 minutes
-    timeout 1800 tshark -i wlan0 -f "arp" -w $FILENAME
-done
+# Capture ARP packets for 5 minutes
+timeout 60 tshark -i wlan0 -f "arp or icmp or udp port 67 or udp port 53 or udp port 1900 or udp port 5353 or ether proto 0x88cc or ether proto 0x2000 or ether proto 0x6003" -w $FILENAME
