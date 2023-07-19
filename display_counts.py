1bd8003c1c0d4565bcae9566e2ce423e3230a80e
FelixGrabler <felix.grabler@eet.energy>
2023-07-19 16:01:36 +0200

    made better

--- display_counts.py
+++ display_counts.py
@@ -1,52 +1,49 @@
 import os
 import sqlite3
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-import numpy as np
+import matplotlib.dates as mdates
 import pandas as pd
-from datetime import datetime
 
 DATABASE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "count.db")
 
 
 def format_timestamp(timestamp):
-    dt = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
-    return dt.strftime("%d.%m %H:%M")
-
-
-def sample_ticks_and_labels(x_ticks, labels, sample_size):
-    if len(x_ticks) > sample_size:
-        step_size = len(x_ticks) // sample_size
-        x_ticks = x_ticks[::step_size]
-        labels = labels[::step_size]
-    return x_ticks, labels
+    return timestamp.strftime("%d.%m %H:%M")
 
 
 def analyze_db():
     with sqlite3.connect(DATABASE) as conn:
         df = pd.read_sql_query("SELECT * FROM mac_counts", conn)
 
-    df_sorted = df.sort_values(by="timestamp")
+    # Convert 'timestamp' to datetime and set as index
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
+    df.set_index("timestamp", inplace=True)
+
+    # Create new datetime index with all half-hour intervals
+    new_index = pd.date_range(start=df.index.min(), end=df.index.max(), freq="30T")
+
+    # Reindex dataframe to include all half-hour intervals
+    df = df.reindex(new_index)
 
     fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(df_sorted["timestamp"], df_sorted["count"], marker="o")
-    ax.grid(True)  # Adding grid
+    ax.plot(df.index, df["count"], marker="o")
 
     ax.set_title("Count of MAC Addresses Over Time")
     ax.set_xlabel("Timestamp")
     ax.set_ylabel("Count")
 
-    x_ticks = np.arange(len(df_sorted["timestamp"]))
-    formatted_labels = [
-        format_timestamp(timestamp) for timestamp in df_sorted["timestamp"]
-    ]
-    x_ticks, formatted_labels = sample_ticks_and_labels(x_ticks, formatted_labels, 100)
-    ax.set_xticks(x_ticks)
-    ax.set_xticklabels(formatted_labels, rotation=45, ha="right")
+    # Set x-axis labels to only show labels for every third hour
+    ax.xaxis.set_major_locator(mdates.HourLocator(interval=3))
+    ax.xaxis.set_minor_locator(
+        mdates.HourLocator(interval=1)
+    )  # Set minor ticks every hour for the grid
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%d.%m %H:%M"))
+    ax.grid(
+        True, which="both"
+    )  # Enable grid for both major (every third hour) and minor (every hour) ticks
 
-    ax.xaxis.set_major_locator(
-        ticker.MultipleLocator(2)
-    )  # Add grid lines every 2 datasets, i.e., every hour
+    # Rotate x-axis labels for better readability
+    plt.xticks(rotation=45)
 
     fig.tight_layout()
     plt.show()  # Show the plot
